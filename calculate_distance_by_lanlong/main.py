4d78ea08eec9c2e007215efc0eef29e87ebd2cee
Koichi Nishiyama <nishiyama4477@gmail.com>
2021-04-11 17:51:06 +0900

    commit１

--- calculate_distance_by_lanlong/main.py
+++ calculate_distance_by_lanlong/main.py
@@ -1,43 +1,48 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, session, redirect, url_for, flash
 from flask_bootstrap import Bootstrap
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField
-from wtforms.validators import DataRequired
+from flask_moment import Moment
+from datetime import datetime
+from wtforms import FloatField,SubmitField
+from wtforms.validators import DataRequired, NumberRange
 from geopy.distance import geodesic
-
 
 app = Flask(__name__)
 bootstrap = Bootstrap(app)
+moment  = Moment(app)
 app.config['SECRET_KEY'] = 'hard to guess string'
 
 class NameForm(FlaskForm):
-    Alat = StringField('What is A\'s latitude?', validators=[DataRequired()])
-    Along = StringField('What is A\'s longtitude?', validators=[DataRequired()])
-    Blat = StringField('What is B\'s latitude?', validators=[DataRequired()])
-    Blong = StringField('What is B\'s longtitude?', validators=[DataRequired()])
+    Alat = FloatField('What is A\'s latitude?', validators=[DataRequired(), NumberRange(min=-90, max=90, message='Latitude is in between -90 to 90')])
+    Along = FloatField('What is A\'s longtitude?', validators=[DataRequired(), NumberRange(min=-180, max=180, message='Longitude is in between -180 to 180')])
+    Blat = FloatField('What is B\'s latitude?', validators=[DataRequired(), NumberRange(min=-90, max=90, message='Latitude is in between -90 to 90')])
+    Blong = FloatField('What is B\'s longtitude?', validators=[DataRequired(), NumberRange(min=-180, max=180, message='Longitude is in between -180 to 180')])
     submit = SubmitField('Submit')
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
-    dis = None
     form = NameForm()
     if form.validate_on_submit():
         Alat = form.Alat.data
         Along = form.Along.data
         Blat = form.Blat.data
         Blong = form.Blong.data
+        if Alat == Along == Blat == Blong:
+            session['A'] = Alat, Along
+            session['B'] = Blat, Blong
+            session['dis'] = str(0)
+        else:
+            session['A'] = Alat, Along
+            session['B'] = Blat, Blong
+            session['dis'] = geodesic(session['A'], session['B']).km
 
-        A = Alat, Along
-        B = Blat, Blong
+        # old_dis = session.get('dis')
+        # if old_dis is not None and old_dis != geodesic(A, B).km:
+        #     flash('Looks like you have changed your location!')
+        # session['dis'] = geodesic(A,B).km
 
-        dis = geodesic(A, B).km
-
-
-        form.Alat.data = ''
-        form.Along.data = ''
-        form.Blat.data = ''
-        form.Blong.data = ''
-    return render_template('main.html', form=form, dis = dis)
+        return redirect(url_for('index'))
+    return render_template('main.html', form=form, dis=session.get('dis'), current_time=datetime.utcnow())
 
 
 if __name__ == '__main__':
